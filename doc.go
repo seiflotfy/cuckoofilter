0b2e6edc0f1f05f578e474b42f42d80541357207
Seif Lotfy <seif.lotfy@gmail.com>
2015-07-26 23:55:50 +0200

    Fix doc.go

--- doc.go
+++ doc.go
@@ -26,8 +26,10 @@ While Bloom filters are well-known space-efficient data structures to serve quer
 Cuckoo filters provide the ﬂexibility to add and remove items dynamically. A cuckoo filter is based on cuckoo hashing (and therefore named as cuckoo filter). It is essentially a cuckoo hash table storing each key's fingerprint. Cuckoo hash tables can be highly compact, thus a cuckoo filter could use less space than conventional Bloom ﬁlters, for applications that require low false positive rates (< 3%).
 
 For details about the algorithm and citations please use this article:
+
 "Cuckoo Filter: Better Than Bloom" by Bin Fan, Dave Andersen and Michael Kaminsky
 (https://www.cs.cmu.edu/~dga/papers/cuckoo-conext2014.pdf)
 
-Note:This implementation uses a a static bucket size of 4 fingerprints and a fingerprint size of 1 byte based on my understanding of an optimal bucket/fingerprint/size ratio from the aforementioned paper.*/
+Note:
+This implementation uses a a static bucket size of 4 fingerprints and a fingerprint size of 1 byte based on my understanding of an optimal bucket/fingerprint/size ratio from the aforementioned paper.*/
 package cuckoofilter
