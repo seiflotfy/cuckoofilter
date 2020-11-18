21c6f98b87cd1ede01371b96132e37c6cd1e54e4
wbzl771398张磊 <wb-zl771398@alibaba-inc.com>
2020-11-18 14:02:11 +0800

    support scalable by add new scalable_cuckoofilter.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/seven4x/cuckoofilter
+
+go 1.15
+
+require (
+	github.com/dgryski/go-metro v0.0.0-20200812162917-85c65e2d0165
+	github.com/stretchr/testify v1.6.1
+)
---
+++ scalable_cuckoofilter_test.go
@@ -0,0 +1,43 @@
+package cuckoo
+
+import (
+	"strconv"
+	"testing"
+)
+import "github.com/stretchr/testify/assert"
+
+func TestNormalUse(t *testing.T) {
+	filter := NewScalableCuckooFilter()
+	for i := 0; i < 100000; i++ {
+		filter.Insert([]byte("NewScalableCuckooFilter_" + strconv.Itoa(i)))
+	}
+	testStr := []byte("NewScalableCuckooFilter")
+	b := filter.Insert(testStr)
+	assert.True(t, b)
+	b = filter.Lookup(testStr)
+	assert.True(t, b)
+	b = filter.Delete(testStr)
+	assert.True(t, b)
+	b = filter.Lookup(testStr)
+	assert.False(t, b)
+	b = filter.Lookup([]byte("NewScalableCuckooFilter_233"))
+	assert.True(t, b)
+	b = filter.InsertUnique([]byte("NewScalableCuckooFilter_599"))
+	assert.False(t, b)
+}
+
+func TestScalableCuckooFilter_DecodeEncode(t *testing.T) {
+	filter := NewScalableCuckooFilter()
+	for i := 0; i < 100000; i++ {
+		filter.Insert([]byte("NewScalableCuckooFilter_" + strconv.Itoa(i)))
+	}
+	bytes := filter.Encode()
+	decodeFilter, err := DecodeScalableFilter(bytes)
+	assert.Nil(t, err)
+	b := decodeFilter.Lookup([]byte("NewScalableCuckooFilter_233"))
+	assert.True(t, b)
+	for i, f := range decodeFilter.filters {
+		assert.Equal(t, f.count, filter.filters[i].count)
+	}
+
+}
